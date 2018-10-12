5ad9e90a32eabbea699514229cd5d1f39ab5bf6b
velmurugan <velmurugan.sw@gmail.com>
2018-10-13 02:35:48 +0530

    Added decryption code

--- breakingDES.py
+++ breakingDES.py
@@ -1,3 +1,13 @@
+"""
+
+OUTPUT:
+Plaintext length 32
+Ciphertext length 32
+Total number of decryption per second :  44620.255319148935
+Time required for 2**55 decryption is approximately 9345531 days
+"""
+
+
 # DES has a fixed block size of 32 byte and its key are 64 bit long
 # Performance analysis : https://www.cse.wustl.edu/~jain/cse567-06/ftp/encryption_perf/
 # https://www.programcreek.com/python/example/101304/Crypto.Cipher.DES.MODE_ECB
@@ -5,7 +15,6 @@
 # 2**55
 # print(2**55)
 import time
-from datetime import datetime, timedelta
 from Crypto.Cipher import DES
 from Crypto import Random
 iv=Random.get_random_bytes(8)
@@ -15,8 +24,7 @@ def runtime(func):
         st =time.time()
         result = func(*args)
         et = time.time()
-        t=et-st
-        print(t)
+        t=(et-st)
         return t,result
     return wrapper
 
@@ -43,18 +51,16 @@ def decrypt(ctext,key):
     des2=DES.new(key,DES.MODE_OFB,iv)
     plain_text = des2.decrypt(ctext)
     return plain_text
-text='32 byte plain text data for test'
-print(len(text.encode('utf-8')))
+text='32byte length plain text for tst'
 
 def measureIt(t):
-    d=t*2**55
-    print(d)
-    print(d/(60*60*24))
-    # d = timedelta(t*2**55)
-    # d_time = datetime(1,1,1)+d
-    # print("%d:%d:%d:%d" % (d_time.day - 1, d_time.hour, d_time.minute, d_time.second))
+    num_of_decryption_per_sec = 1/t
+    print("Total number of decryption per second : ",num_of_decryption_per_sec)
+    time_req_for_given_trans = 2**55/num_of_decryption_per_sec
+    print("Time required for 2**55 decryption is approximately {} days ".format(round(time_req_for_given_trans/(60*60*24))))
 
 encrypt_time,ctxt = encrypt(text,'abcdefgh')
 decrypt_time,ptxt = decrypt(ctxt,'abcdefgh')
-print(decrypt_time)
+print("Plaintext length {}".format(len(text.encode('utf-8'))))
+print("Ciphertext length {}".format(len(ctxt)))
 measureIt(decrypt_time)
